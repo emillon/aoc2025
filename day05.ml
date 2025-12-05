86850a1fa247959ad4fad4cbf9ed107da217bd58
Etienne Millon <me@emillon.org>
2025-12-05 12:08:16 +0100

    cleanup

--- day05.ml
+++ day05.ml
@@ -1,9 +1,3 @@
-type t =
-  { ranges : (int * int) list
-  ; ingredients : int list
-  }
-[@@deriving sexp]
-
 let parse =
   let open Angstrom in
   let open Parsing_util in
@@ -17,21 +11,22 @@ let parse =
      let+ ranges = many1 (range <* end_of_line)
      and+ _ = end_of_line
      and+ ingredients = many1 (number <* end_of_line) in
-     { ranges; ingredients })
+     ranges, ingredients)
+;;
+
+let make_diet ranges =
+  List.fold ranges ~init:Diet.Int.empty ~f:(fun d (a, b) ->
+    let i = Diet.Int.Interval.make a b in
+    Diet.Int.add i d)
 ;;
 
 let f1 s =
-  let t = parse s in
-  List.count t.ingredients ~f:(fun n ->
-    List.exists t.ranges ~f:(fun (a, b) -> a <= n && n <= b))
+  let ranges, ingredients = parse s in
+  let d = make_diet ranges in
+  List.count ingredients ~f:(fun n -> Diet.Int.mem n d)
 ;;
 
-let f2 s =
-  let t = parse s in
-  List.fold t.ranges ~init:Diet.Int.empty ~f:(fun d (a, b) ->
-    Diet.Int.add (Diet.Int.Interval.make a b) d)
-  |> Diet.Int.cardinal
-;;
+let f2 s = parse s |> fst |> make_diet |> Diet.Int.cardinal
 
 let sample =
   String.concat_lines
@@ -39,13 +34,8 @@ let sample =
 ;;
 
 let%expect_test _ =
-  print_s [%message (parse sample : t) (f1 sample : int) (f2 sample : int)];
-  [%expect
-    {|
-    (("parse sample"
-      ((ranges ((3 5) (10 14) (16 20) (12 18))) (ingredients (1 5 8 11 17 32))))
-     ("f1 sample" 3) ("f2 sample" 14))
-    |}]
+  print_s [%message (f1 sample : int) (f2 sample : int)];
+  [%expect {| (("f1 sample" 3) ("f2 sample" 14)) |}]
 ;;
 
 let run () = Run.run ~f1 ~f2 Day05_input.data
